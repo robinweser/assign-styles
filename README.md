af4628af813d1f639f22f9e4337c9954a14b79c1
rofrischmann <robin@rofrischmann.de>
2015-07-23 22:50:44 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# assign-styles
+```sh
+npm install assign-styles
+```
+ES6 Polyfill for `Object.assign` that respects **!important** notation.<br>
+This does the same as [object-assign](https://github.com/sindresorhus/object-assign), but was made especially for style objects as they're used e.g. in React to define inline-styles.
+
+# License
+**assign-styles** is licensed under the [MIT License](http://opensource.org/licenses/MIT).<br>Created with ♥ by [@rofrischmann](http://rofrischmann.de).
---
+++ src/index.js
@@ -0,0 +1,35 @@
+/**
+ * Assigns two styles and optionally overwrites existing values
+ * Built to assign inline-style objects and respects CSS's !important annotation
+ * @param {Object} styles - style objects which get merged together
+ * Note: The first style object will serve as base
+ * Existing values will get overwritten by default
+ */
+export default function assignStyles(...styles) {
+	let property;
+
+	let newStyles = styles.splice(1);
+	let base = styles[0];
+
+	newStyles.forEach(styleObj => {
+		if (styleObj) {
+			for (property in styleObj) {
+				if (base.hasOwnProperty(property) && isImportant(base[property])) {
+					base[property] = base[property];
+				} else {
+					base[property] = styleObj[property];
+				}
+			}
+		}
+	})
+
+	return base;
+}
+
+/**
+ * Checks if a property value is an css important rule with !important
+ * @param {string} property - property thats value gets checked 
+ */
+function isImportant(value) {
+	return typeof value == 'string' && value.toLowerCase().indexOf('!important') > -1;
+}
