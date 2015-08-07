23c27cf3c314e10796ea138c2c85b430694ac833
rofrischmann <robin@rofrischmann.de>
2015-08-07 09:35:00 +0200

    added tests

---
+++ test/index.js
@@ -0,0 +1,42 @@
+import assignStyles from '../lib/index';
+import {expect} from 'chai';
+
+
+describe('Assigning styles', () => {
+	
+	let emptyAssign = assignStyles()
+	it('should return undefined if arguments are empty', () => {
+		expect(emptyAssign).to.equal(undefined);
+	});
+	
+	let onlyOne = assignStyles({foo: 1, bar: 2})
+	it('should return the object if only one is passed', () => {
+		expect(onlyOne).to.eql({foo: 1, bar: 2});
+	});
+	
+	let merged = assignStyles({foo: 1}, {bar: 2})
+	it('should merge 2 objects', () => {
+		expect(merged).to.eql({foo: 1, bar: 2});
+	});
+	
+	let overwritten = assignStyles({foo: 1}, {foo: 3, bar: 2})
+	it('should overwrite existing values', () => {
+		expect(overwritten).to.eql({foo: 3, bar: 2});
+	});
+	
+	let important = assignStyles({foo: '1 !important'}, {foo: 3, bar: 2})
+	it('should not overwrite existing values with !important flag', () => {
+		expect(important).to.eql({foo: '1 !important', bar: 2});
+	});
+	
+	let obj1 = {foo: 1, bla: 3};
+	let obj2 = {bar: 3};
+	
+	let newObj = assignStyles({}, obj1, obj2);
+	newObj.bla = 4;
+	it('should create a new object that is not related to the source objects', () => {
+		expect(obj1).to.eql({foo: 1, bla: 3});
+		expect(obj2).to.eql({bar : 3});
+		expect(newObj).to.eql({foo: 1, bar: 3, bla: 4});
+	});
+});
